c6a392f063934718c625fafb12ad055006ee7a3d
PxUknDevelopment <88362177+PxUknDevelopment@users.noreply.github.com>
2021-08-03 13:50:58 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,19 +1,19 @@
-RegisterCommand('help', function()
+RegisterCommand('rules', function()
     TriggerEvent('chat:addMessage', {
         color = {255.0,0},
         multiline = true,
-        args = {'[SERVER]', 'Join the Discord for further help: discord.gg/example'}
+        args = {'[SERVER]', 'Here at [Insert Server Name], we strive for realistic roleplay and serious roleplay. Please make sure you are following and abiding by our rules within your time playing here. If you do not know how to find our rules, go to [insert rules link without the brackets]. Have fun!'}
     })
-    TriggerServerEvent('srp:help')
-end) -- /help
+    TriggerServerEvent('srp:rules')
+end) -- /rules
 
 
 RegisterNetEvent('srp:sendNessage')
 AddEventHandler('srp:sendMessage' function()
         color = {255.0,0},
         multiline = true,
-        args = {'[SERVER]', 'Join the Discord for further help: discord.gg/example'}
+        args = {'[SERVER]', 'Here at [Insert Server Name], we strive for realistic roleplay and serious roleplay. Please make sure you are following and abiding by our rules within your time playing here. If you do not know how to find our rules, go to [insert rules link without the brackets]. Have fun!'}
     })
 end)
 
--- [SERVER] : Join the Discord for further help: discord.gg/example
+-- [SERVER] : Here at [Insert Server Name], we strive for realistic roleplay and serious roleplay. Please make sure you are following and abiding by our rules within your time playing here. If you do not know how to find our rules, go to [insert rules link without the brackets]. Have fun!
