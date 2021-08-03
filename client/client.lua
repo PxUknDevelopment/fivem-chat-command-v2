d6f993804f636a779e3a316d21937d43df2fc504
PxUknDevelopment <88362177+PxUknDevelopment@users.noreply.github.com>
2021-08-03 13:29:45 -0400

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -1,5 +1,19 @@
 RegisterCommand('help', function()
     TriggerEvent('chat:addMessage', {
-        color = {255.0,0}
+        color = {255.0,0},
+        multiline = true,
+        args = {'[SERVER]', 'Join the Discord for further help: discord.gg/example'}
     })
+    TriggerServerEvent('srp:help')
 end) -- /help
+
+
+RegisterNetEvent('srp:sendNessage')
+AddEventHandler('srp:sendMessage' function()
+        color = {255.0,0},
+        multiline = true,
+        args = {'[SERVER]', 'Join the Discord for further help: discord.gg/example'}
+    })
+end)
+
+-- [SERVER] : Join the Discord for further help: discord.gg/example
